4b5018840f2ffe33ebc1db63d88434cacf2caedf
Leo Sabin <leo@tiaslaptop.bloomfield.org>
2016-03-28 19:51:50 -0400

    Initial commit

---
+++ src/com/example/Pitcher_Scouting/Page2.java
@@ -0,0 +1,15 @@
+package com.example.Pitcher_Scouting;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by leo on 3/28/16.
+ */
+public class Page2 extends Activity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.page2);
+    }
+}
